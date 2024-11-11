f0b664e987549d7ff3b24213f3abb17c503c7f99
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-11-11 23:02:54 +0800

    Update SceMotion.cpp

--- vita3k/modules/SceDriverUser/SceMotion.cpp
+++ vita3k/modules/SceDriverUser/SceMotion.cpp
@@ -82,27 +82,34 @@ EXPORT(int, sceMotionGetSensorState, SceMotionSensorState *sensorState, int numR
     if (!sensorState)
         return RET_ERROR(SCE_MOTION_ERROR_NULL_PARAMETER);
 
-    if (emuenv.ctrl.has_motion_support || emuenv.motion.has_device_motion_support) {
-        std::lock_guard<std::mutex> guard(emuenv.motion.mutex);
-        sensorState->accelerometer = get_acceleration(emuenv.motion);
-        sensorState->gyro = get_gyroscope(emuenv.motion);
-
-        sensorState->timestamp = emuenv.motion.last_accel_timestamp;
-        sensorState->counter = emuenv.motion.last_counter;
-        sensorState->hostTimestamp = sensorState->timestamp;
-        sensorState->dataInfo = 0;
-    } else {
-        // some default values
-        memset(sensorState, 0, sizeof(*sensorState));
-        sensorState->accelerometer.z = -1.0;
-
-        std::chrono::time_point<std::chrono::steady_clock> ts = std::chrono::steady_clock::now();
-        uint64_t timestamp = std::chrono::duration_cast<std::chrono::microseconds>(ts.time_since_epoch()).count();
-        sensorState->timestamp = timestamp;
-        sensorState->hostTimestamp = timestamp;
-
-        sensorState->counter = emuenv.motion.last_counter++;
-    }
+        if (emuenv.ctrl.has_motion_support || emuenv.motion.has_device_motion_support && !emuenv.cfg.disable_motion) {
+            std::lock_guard<std::mutex> guard(emuenv.motion.mutex);
+            sensorState->accelerometer = get_acceleration(emuenv.motion);
+            sensorState->gyro = get_gyroscope(emuenv.motion);
+            if(emuenv.cfg.invert_gyro){
+               sensorState->gyro.x = sensorState->gyro.x * -1;
+               sensorState->gyro.y = sensorState->gyro.y * -1;
+               sensorState->gyro.z = sensorState->gyro.z * -1;
+            }
+            sensorState->timestamp = emuenv.motion.last_accel_timestamp;
+            sensorState->counter = emuenv.motion.last_counter;
+            sensorState->hostTimestamp = sensorState->timestamp;
+            sensorState->dataInfo = 0;
+        } else {
+            // some default values
+            memset(sensorState, 0, sizeof(*sensorState));
+            sensorState->accelerometer.z = -1.0;
+            sensorState->accelerometer.x = 0;
+            sensorState->accelerometer.y = 0;
+            sensorState->gyro = {0,0,0};
+            
+            std::chrono::time_point<std::chrono::steady_clock> ts = std::chrono::steady_clock::now();
+            uint64_t timestamp = std::chrono::duration_cast<std::chrono::microseconds>(ts.time_since_epoch()).count();
+            sensorState->timestamp = timestamp;
+            sensorState->hostTimestamp = timestamp;
+    
+            sensorState->counter = emuenv.motion.last_counter++;
+        }
 
     for (int i = 1; i < numRecords; i++)
         sensorState[i] = sensorState[0];
@@ -115,43 +122,50 @@ EXPORT(int, sceMotionGetState, SceMotionState *motionState) {
     if (!motionState)
         return RET_ERROR(SCE_MOTION_ERROR_NULL_PARAMETER);
 
-    if (emuenv.ctrl.has_motion_support || emuenv.motion.has_device_motion_support) {
-        std::lock_guard<std::mutex> guard(emuenv.motion.mutex);
-        motionState->timestamp = emuenv.motion.last_accel_timestamp;
-
-        motionState->acceleration = get_acceleration(emuenv.motion);
-        motionState->angularVelocity = get_gyroscope(emuenv.motion);
-
-        Util::Quaternion dev_quat = get_orientation(emuenv.motion);
-
-        static_assert(sizeof(motionState->deviceQuat) == sizeof(dev_quat));
-        memcpy(&motionState->deviceQuat, &dev_quat, sizeof(motionState->deviceQuat));
-
-        *reinterpret_cast<decltype(dev_quat.ToMatrix()) *>(&motionState->rotationMatrix) = dev_quat.ToMatrix();
-        // not right, but we can't do better without a magnetometer
-        memcpy(&motionState->nedMatrix, &motionState->rotationMatrix, sizeof(motionState->nedMatrix));
-
-        motionState->hostTimestamp = motionState->timestamp;
-        // set it as unstable because we don't have one
-        motionState->magnFieldStability = SCE_MOTION_MAGNETIC_FIELD_UNSTABLE;
-        motionState->dataInfo = 0;
-    } else {
-        // put some default values
-        memset(motionState, 0, sizeof(SceMotionState));
-
-        std::chrono::time_point<std::chrono::steady_clock> ts = std::chrono::steady_clock::now();
-        uint64_t timestamp = std::chrono::duration_cast<std::chrono::microseconds>(ts.time_since_epoch()).count();
-        motionState->timestamp = timestamp;
-        motionState->hostTimestamp = timestamp;
-
-        motionState->acceleration.z = -1.0;
-        motionState->deviceQuat.z = 1;
-        for (int i = 0; i < 4; i++) {
-            // identity matrices
-            reinterpret_cast<float *>(&motionState->rotationMatrix.x.x)[i * 4 + i] = 1;
-            reinterpret_cast<float *>(&motionState->nedMatrix.x.x)[i * 4 + i] = 1;
+        if (emuenv.ctrl.has_motion_support || emuenv.motion.has_device_motion_support && !emuenv.cfg.disable_motion) {
+            std::lock_guard<std::mutex> guard(emuenv.motion.mutex);
+            motionState->timestamp = emuenv.motion.last_accel_timestamp;
+    
+            motionState->acceleration = get_acceleration(emuenv.motion);
+            motionState->angularVelocity = get_gyroscope(emuenv.motion);
+            if(emuenv.cfg.invert_gyro){
+               motionState->angularVelocity.x = motionState->angularVelocity.x * -1;
+               motionState->angularVelocity.y = motionState->angularVelocity.y * -1;
+               motionState->angularVelocity.z = motionState->angularVelocity.z * -1;
+            }
+            Util::Quaternion dev_quat = get_orientation(emuenv.motion);
+    
+            static_assert(sizeof(motionState->deviceQuat) == sizeof(dev_quat));
+            memcpy(&motionState->deviceQuat, &dev_quat, sizeof(motionState->deviceQuat));
+    
+            *reinterpret_cast<decltype(dev_quat.ToMatrix()) *>(&motionState->rotationMatrix) = dev_quat.ToMatrix();
+            // not right, but we can't do better without a magnetometer
+            memcpy(&motionState->nedMatrix, &motionState->rotationMatrix, sizeof(motionState->nedMatrix));
+    
+            motionState->hostTimestamp = motionState->timestamp;
+            // set it as unstable because we don't have one
+            motionState->magnFieldStability = SCE_MOTION_MAGNETIC_FIELD_UNSTABLE;
+            motionState->dataInfo = 0;
+        } else {
+            // put some default values
+            memset(motionState, 0, sizeof(SceMotionState));
+    
+            std::chrono::time_point<std::chrono::steady_clock> ts = std::chrono::steady_clock::now();
+            uint64_t timestamp = std::chrono::duration_cast<std::chrono::microseconds>(ts.time_since_epoch()).count();
+            motionState->timestamp = timestamp;
+            motionState->hostTimestamp = timestamp;
+    
+            motionState->acceleration.z = -1.0;
+            motionState->acceleration.y = 0;
+            motionState->acceleration.x = 0;
+            motionState->angularVelocity = {0,0,0};
+            motionState->deviceQuat.z = 1;
+            for (uint8_t i = 0; i < 4; i++) {
+                // identity matrices
+                reinterpret_cast<float *>(&motionState->rotationMatrix.x.x)[i * 4 + i] = 1;
+                reinterpret_cast<float *>(&motionState->nedMatrix.x.x)[i * 4 + i] = 1;
+            }
         }
-    }
 
     return 0;
 }
