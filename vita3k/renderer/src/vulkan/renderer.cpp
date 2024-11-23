3c1a274142109efc9933a4700462ffc2e0d73d03
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-11-23 23:57:58 +0800

    Update renderer.cpp

--- vita3k/renderer/src/vulkan/renderer.cpp
+++ vita3k/renderer/src/vulkan/renderer.cpp
@@ -497,7 +497,6 @@ bool VKState::create(SDL_Window *window, std::unique_ptr<renderer::State> &state
 
         LOG_INFO("Vulkan device: {}", physical_device_properties.deviceName.data());
         LOG_INFO("Driver version: {}", get_driver_version(physical_device_properties.vendorID, physical_device_properties.driverVersion));
-	LOG_INFO("Vulkan memory size: {}", physical_device_memory.data());
     }
 
     if (support_custom_drivers()) {
