4107a5d3ac9afa1deef9c003f9985112ff3c479e
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-11-26 10:22:48 +0800

    Update pipeline_cache.cpp

--- vita3k/renderer/src/vulkan/pipeline_cache.cpp
+++ vita3k/renderer/src/vulkan/pipeline_cache.cpp
@@ -149,7 +149,7 @@ void PipelineCache::init(bool support_rasterized_order_access) {
 
         // first vertex
         std::array<vk::DescriptorSetLayoutBinding, 16> layout_bindings;
-        for (uint32_t i = 0; i < 16; i++) {
+        for (uint8_t i = 0; i < 16; i++) {
             layout_bindings[i] = {
                 .binding = i,
                 .descriptorType = vk::DescriptorType::eCombinedImageSampler,
@@ -157,7 +157,7 @@ void PipelineCache::init(bool support_rasterized_order_access) {
                 .stageFlags = vk::ShaderStageFlagBits::eVertex
             };
         }
-        for (uint32_t i = 1; i <= 16; i++) {
+        for (uint8_t i = 1; i <= 16; i++) {
             vk::DescriptorSetLayoutCreateInfo descriptor_info{
                 .bindingCount = i,
                 .pBindings = layout_bindings.data()
@@ -166,10 +166,10 @@ void PipelineCache::init(bool support_rasterized_order_access) {
         }
 
         // then fragment
-        for (uint32_t i = 0; i < 16; i++) {
+        for (uint8_t i = 0; i < 16; i++) {
             layout_bindings[i].stageFlags = vk::ShaderStageFlagBits::eFragment;
         }
-        for (uint32_t i = 1; i <= 16; i++) {
+        for (uint8_t i = 1; i <= 16; i++) {
             vk::DescriptorSetLayoutCreateInfo descriptor_info{
                 .bindingCount = i,
                 .pBindings = layout_bindings.data()
@@ -179,8 +179,8 @@ void PipelineCache::init(bool support_rasterized_order_access) {
     }
 
     // compute all possible pipeline layouts
-    for (uint32_t vert_texture_count = 0; vert_texture_count <= 16; vert_texture_count++) {
-        for (uint32_t frag_texture_count = 0; frag_texture_count <= 16; frag_texture_count++) {
+    for (uint8_t vert_texture_count = 0; vert_texture_count <= 16; vert_texture_count++) {
+        for (uint8_t frag_texture_count = 0; frag_texture_count <= 16; frag_texture_count++) {
             vk::PipelineLayoutCreateInfo layout_info{};
             vk::DescriptorSetLayout set_layouts[] = { uniforms_layout, attachments_layout, vertex_textures_layout[vert_texture_count], fragment_textures_layout[frag_texture_count] };
             layout_info.setSetLayouts(set_layouts);
@@ -188,6 +188,7 @@ void PipelineCache::init(bool support_rasterized_order_access) {
         }
     }
 
+#ifndef ANDROID
     {
         // look for rgb vertex attribute support
         // we need to look at each format because it is not the same for all usual 3-component formats (checked on AMD Radeon HD 7800)
@@ -221,7 +222,8 @@ void PipelineCache::init(bool support_rasterized_order_access) {
         }
         state.features.support_rgb_attributes = unsupported_rgb_vertex_attribute_formats.empty();
     }
-
+#endif
+    
     support_coherent_framebuffer_fetch = support_rasterized_order_access;
 
     const int nb_logical_threads = SDL_GetCPUCount();
@@ -794,7 +796,7 @@ vk::Pipeline PipelineCache::compile_pipeline(SceGxmPrimitiveType type, vk::Rende
         .depthTestEnable = VK_TRUE,
         .depthWriteEnable = (record.front_depth_write_mode == SCE_GXM_DEPTH_WRITE_ENABLED),
         .depthCompareOp = translate_depth_func(record.front_depth_func),
-        .depthBoundsTestEnable = VK_FALSE,
+        .depthBoundsTestEnable = VK_TRUE,
         .stencilTestEnable = VK_TRUE,
         .front = convert_op_state(record.front_stencil_state_op),
         .back = convert_op_state(two_sided ? record.back_stencil_state_op : record.front_stencil_state_op)
