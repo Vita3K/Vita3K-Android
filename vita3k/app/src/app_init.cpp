d72d8bca3a695f34ccc1caf0355485f36f44d298
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-08 23:01:11 +0800

    revert app_init.cpp

--- vita3k/app/src/app_init.cpp
+++ vita3k/app/src/app_init.cpp
@@ -604,10 +604,7 @@ void switch_state(EmuEnvState &emuenv, const bool pause) {
     else {
 #ifdef ANDROID
         emuenv.display.imgui_render = false;
-        // hide virtual controller when controller connected (since virtual controller didn't work when controller connected)
-        if(SDL_NumJoysticks() > 1) 
-            gui::set_controller_overlay_state(0);
-        else if (emuenv.cfg.enable_gamepad_overlay)
+        if (emuenv.cfg.enable_gamepad_overlay)
             gui::set_controller_overlay_state(gui::get_overlay_display_mask(emuenv.cfg));
 #endif
 
