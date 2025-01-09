2280a85239fc5d80fc8573a1a4d33b9a5f42641d
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-10 00:36:59 +0800

    Update SceMotion.cpp

--- vita3k/modules/SceDriverUser/SceMotion.cpp
+++ vita3k/modules/SceDriverUser/SceMotion.cpp
@@ -87,9 +87,9 @@ EXPORT(int, sceMotionGetSensorState, SceMotionSensorState *sensorState, int numR
         sensorState->accelerometer = get_acceleration(emuenv.motion);
         sensorState->gyro = get_gyroscope(emuenv.motion);
         if(emuenv.cfg.calibrate_gyro){
-           sensorState->gyro.x = emuenv.cfg.controller_gyro_calibration[0];
-           sensorState->gyro.y = emuenv.cfg.controller_gyro_calibration[1];
-           sensorState->gyro.z = emuenv.cfg.controller_gyro_calibration[2];
+           sensorState->gyro.x = sensorState->gyro.x + emuenv.cfg.controller_gyro_calibration[0];
+           sensorState->gyro.y = sensorState->gyro.y + emuenv.cfg.controller_gyro_calibration[1];
+           sensorState->gyro.z = sensorState->gyro.z + emuenv.cfg.controller_gyro_calibration[2];
         }
         if(emuenv.cfg.invert_gyro){
            sensorState->gyro.x = sensorState->gyro.x * -1;
