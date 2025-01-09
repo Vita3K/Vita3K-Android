ca121eaa0060c3f82f76b54e75971c1ede48447b
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-10 01:07:36 +0800

    Update initial_setup.cpp

--- vita3k/gui/src/initial_setup.cpp
+++ vita3k/gui/src/initial_setup.cpp
@@ -113,6 +113,11 @@ void draw_initial_setup(GuiState &gui, EmuEnvState &emuenv) {
     ImGui::SetWindowFontScale(1.4f * RES_SCALE.x);
     ImGui::SetCursorPosY(94.f * SCALE.y);
     ImGui::Separator();
+    
+#ifdef ANDROID
+    constexpr char path_warning[] = "Using a different path requires additional permissions";
+#endif
+    
     switch (setup) {
     case SELECT_LANGUAGE:
         title_str = lang["select_language"];
@@ -143,6 +148,9 @@ void draw_initial_setup(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::PopStyleVar();
         ImGui::PopStyleColor(3);
         ImGui::Columns(1);
+#ifdef ANDROID
+        ImGui::ScrollWhenDragging();
+#endif
         ImGui::EndChild();
         break;
     case SELECT_PREF_PATH:
@@ -150,10 +158,14 @@ void draw_initial_setup(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::SetCursorPosY((WINDOW_SIZE.y / 2.f) - ImGui::GetFontSize());
         TextColoredCentered(GUI_COLOR_TEXT_TITLE, lang["current_emu_path"].c_str());
         ImGui::Spacing();
+#ifdef ANDROID
+        ImGui::SetCursorPosX((WINDOW_SIZE.x / 2.f) - (ImGui::CalcTextSize(path_warning).x / 2.f));
+        ImGui::TextColored(ImVec4(0.98f, 0.01f, 0.20f, 1.0f), "%s", path_warning);
+#endif
         TextCentered(emuenv.cfg.pref_path.c_str(), 0);
         ImGui::SetCursorPos(!is_default_path ? ImVec2((WINDOW_SIZE.x / 2.f) - BIG_BUTTON_SIZE.x - (20.f * SCALE.x), BIG_BUTTON_POS.y) : BIG_BUTTON_POS);
         if (ImGui::Button(lang["change_emu_path"].c_str(), BIG_BUTTON_SIZE)) {
-            std::filesystem::path emulator_path = "";
+            fs::path emulator_path = "";
             host::dialog::filesystem::Result result = host::dialog::filesystem::pick_folder(emulator_path);
 
             if ((result == host::dialog::filesystem::Result::SUCCESS) && (emulator_path.native() != emuenv.pref_path.native())) {
