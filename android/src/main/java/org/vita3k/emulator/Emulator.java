a7266bffbd3b8591ce4125fadd2fc246e175f463
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-16 16:35:49 +0800

    Update Emulator.java

--- android/src/main/java/org/vita3k/emulator/Emulator.java
+++ android/src/main/java/org/vita3k/emulator/Emulator.java
@@ -78,13 +78,11 @@ public class Emulator extends SDLActivity
         return mSurface;
     }
 
-    /*
-    @Overridez
+    @Override
     protected void setupLayout(ViewGroup layout){
         super.setupLayout(layout);
         layout.addView(getmOverlay());
     }
-    */
     
     private final String APP_RESTART_PARAMETERS = "AppStartParameters";
 
