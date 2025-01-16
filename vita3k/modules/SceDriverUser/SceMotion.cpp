7f84bc6d3b9c2ac77a3cdd6a879375d564685f59
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-16 18:09:01 +0800

    Update SceMotion.cpp

--- vita3k/modules/SceDriverUser/SceMotion.cpp
+++ vita3k/modules/SceDriverUser/SceMotion.cpp
@@ -21,9 +21,6 @@
 #include <motion/functions.h>
 #include <motion/motion.h>
 
-#include <util/log.h>
-#include <util/string_utils.h>
-
 #include <util/tracy.h>
 TRACY_MODULE_NAME(SceMotion);
 
@@ -89,11 +86,8 @@ EXPORT(int, sceMotionGetSensorState, SceMotionSensorState *sensorState, int numR
         std::lock_guard<std::mutex> guard(emuenv.motion.mutex);
         sensorState->accelerometer = get_acceleration(emuenv.motion);
         sensorState->gyro = get_gyroscope(emuenv.motion);
-
-        LOG_WARN("CTRL JOYSTICK INDEX: {}", emuenv.ctrl.controllers_num);
-        LOG_WARN("CTRL is_virtual_joystick = {}", emuenv.ctrl.is_virtual_joystick);
         
-        if(emuenv.cfg.calibrate_gyro && (emuenv.ctrl.controllers_num != 0 && !emuenv.ctrl.is_virtual_joystick)){
+        if(emuenv.cfg.calibrate_gyro && !emuenv.ctrl.is_virtual_joystick){
            sensorState->gyro.x = sensorState->gyro.x + emuenv.cfg.controller_gyro_calibration[0];
            sensorState->gyro.y = sensorState->gyro.y + emuenv.cfg.controller_gyro_calibration[1];
            sensorState->gyro.z = sensorState->gyro.z + emuenv.cfg.controller_gyro_calibration[2];
@@ -143,7 +137,7 @@ EXPORT(int, sceMotionGetState, SceMotionState *motionState) {
 
         motionState->acceleration = get_acceleration(emuenv.motion);
         motionState->angularVelocity = get_gyroscope(emuenv.motion);
-        if(emuenv.cfg.calibrate_gyro && (emuenv.ctrl.controllers_num != 0 && !emuenv.ctrl.is_virtual_joystick)){
+        if(emuenv.cfg.calibrate_gyro && !emuenv.ctrl.is_virtual_joystick){
            motionState->angularVelocity.x = motionState->angularVelocity.x + emuenv.cfg.controller_gyro_calibration[0];
            motionState->angularVelocity.y = motionState->angularVelocity.y + emuenv.cfg.controller_gyro_calibration[1];
            motionState->angularVelocity.z = motionState->angularVelocity.z + emuenv.cfg.controller_gyro_calibration[2];
