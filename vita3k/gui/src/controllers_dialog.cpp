3ca0872e305d4fd80a24e0230a1618a76395e410
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-17 00:08:43 +0800

    Update controllers_dialog.cpp

--- vita3k/gui/src/controllers_dialog.cpp
+++ vita3k/gui/src/controllers_dialog.cpp
@@ -379,11 +379,11 @@ void draw_controllers_dialog(GuiState &gui, EmuEnvState &emuenv) {
                                 gyro_set = { 0, 0, 0 };
                         if (emuenv.cfg.calibrate_gyro) {
                             ImGui::Spacing();
-                            ImGui::InputFloat(lang["gyro-x"].c_str(), &gyro_set[0], -1, 1, "%.3f");
+                            ImGui::InputFloat(lang["gyro-x"].c_str(), &gyro_set[0], -1, 1, "%.2f");
                             ImGui::Spacing();
-                            ImGui::InputFloat(lang["gyro-y"].c_str(), &gyro_set[1], -1, 1, "%.3f");
+                            ImGui::InputFloat(lang["gyro-y"].c_str(), &gyro_set[1], -1, 1, "%.2f");
                             ImGui::Spacing();
-                            ImGui::InputFloat(lang["gyro-z"].c_str(), &gyro_set[2], -1, 1, "%.3f");
+                            ImGui::InputFloat(lang["gyro-z"].c_str(), &gyro_set[2], -1, 1, "%.2f");
                             ImGui::Spacing();
                             if (ImGui::Button(common["save"].c_str(), BUTTON_SIZE)){
                                gyro_sensor = gyro_set;
