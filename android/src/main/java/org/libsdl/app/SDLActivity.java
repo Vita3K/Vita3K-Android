21c11578340beae5d21d2763cc88b790871d3c33
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-16 17:03:58 +0800

    Update SDLActivity.java

--- android/src/main/java/org/libsdl/app/SDLActivity.java
+++ android/src/main/java/org/libsdl/app/SDLActivity.java
@@ -274,6 +274,10 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
         };
     }
 
+    protected void setupLayout(ViewGroup layout){
+        layout.addView(mSurface);
+    }
+        
     // Load the .so
     public void loadLibraries() {
        for (String lib : getLibraries()) {
