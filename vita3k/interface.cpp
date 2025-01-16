3d4471b8e3fa99e6ec78b4c968e9104784a69659
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-01-16 22:18:33 +0800

    Update interface.cpp

--- vita3k/interface.cpp
+++ vita3k/interface.cpp
@@ -593,9 +593,10 @@ static void take_screenshot(EmuEnvState &emuenv) {
 bool handle_events(EmuEnvState &emuenv, GuiState &gui) {
     refresh_controllers(emuenv.ctrl, emuenv);   
 
-    if(emuenv.ctrl.controllers_num > 0)
+    if(emuenv.ctrl.controllers_num > 0){
+        gui::set_controller_overlay_state(0);
         emuenv.ctrl.is_virtual_joystick = false;
-    else
+    }else
         emuenv.ctrl.is_virtual_joystick = true;
 
     const auto allow_switch_state = !emuenv.io.title_id.empty() && !gui.vita_area.app_close && !gui.vita_area.home_screen && !gui.vita_area.user_management && !gui.configuration_menu.custom_settings_dialog && !gui.configuration_menu.settings_dialog && !gui.controls_menu.controls_dialog && gui::get_sys_apps_state(gui);
