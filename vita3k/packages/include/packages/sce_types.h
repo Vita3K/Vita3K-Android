b9352f1808526dc77caf41f4a8a131b7e15f7bff
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-05 05:46:54 +0800

    Update sce_types.h

--- vita3k/packages/include/packages/sce_types.h
+++ vita3k/packages/include/packages/sce_types.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -652,6 +652,7 @@ public:
 
 void register_keys(KeyStore &SCE_KEYS, int type);
 void extract_fat(const fs::path &partition_path, const std::string &partition, const fs::path &pref_path);
+bool is_self(const fs::path &file_path);
 std::string decompress_segments(const std::vector<uint8_t> &decrypted_data, const uint64_t &size);
 std::tuple<uint64_t, SelfType> get_key_type(std::ifstream &file, const SceHeader &sce_hdr);
 std::vector<SceSegment> get_segments(const uint8_t *input, const SceHeader &sce_hdr, KeyStore &SCE_KEYS, uint64_t sysver = -1, SelfType self_type = static_cast<SelfType>(0), int keytype = 0, const uint8_t *klic = 0);
