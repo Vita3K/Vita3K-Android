0ed39bc0b47f8b23c5eb572d7b9d2beb00caf2a7
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-06 04:37:17 +0800

    Update module_parent.cpp

--- vita3k/modules/module_parent.cpp
+++ vita3k/modules/module_parent.cpp
@@ -239,11 +239,18 @@ SceUID load_module(EmuEnvState &emuenv, const std::string &module_path) {
 
     if (device == VitaIoDevice::app0){
         std::string tmp(module_buffer.begin(), module_buffer.end());
-        LOG_TRACE("LOAD MODULE READ APP FILE:\nGET INFO: module_buffer = {}\nemuenv.pref_path = {}\nemuenv.io.app_path = {}\ntranslated_module_path = {}", tmp, emuenv.pref_path.c_str(), emuenv.io.app_path.c_str(),translated_module_path); 
+        LOG_TRACE("LOAD MODULE READ APP FILE:\nGET INFO: module_buffer = {}",tmp);
+        LOG_TRACE("emuenv.pref_path = {}", emuenv.pref_path.c_str());
+        LOG_TRACE("emuenv.io.app_path = {}", emuenv.io.app_path.c_str());
+        LOG_TRACE("translated_module_path = {}",translated_module_path);
         res = vfs::read_app_file(module_buffer, emuenv.pref_path, emuenv.io.app_path, translated_module_path);
     }else{
         std::string tmp(module_buffer.begin(), module_buffer.end());
-        LOG_TRACE("LOAD MODULE READ FILE:\nGET INFO: device = {}\nmodule_buffer = {}\nemuenv.pref_path = {}\ntranslated_module_path = {}",device_for_icase,tmp,emuenv.pref_path.c_str(),translated_module_path);
+        LOG_TRACE("LOAD MODULE READ FILE:\nGET INFO: device = {}", device_for_icase);
+        LOG_TRACE("module_buffer = {}", tmp);
+        LOG_TRACE("emuenv.pref_path = {}", emuenv.pref_path.c_str());
+        LOG_TRACE("translated_module_path = {}", translated_module_path);
+        
         res = vfs::read_file(device, module_buffer, emuenv.pref_path, translated_module_path);
     }
     if (!res) {
