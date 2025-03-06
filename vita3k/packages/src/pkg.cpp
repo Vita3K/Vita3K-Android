1a3923162a23fb68a20903326a4be5dc102016e5
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-07 06:12:03 +0800

    Update pkg.cpp

--- vita3k/packages/src/pkg.cpp
+++ vita3k/packages/src/pkg.cpp
@@ -57,7 +57,7 @@ int execute(std::string &zrif, fs::path &title_src, fs::path &title_dst, F00DEnc
     return execute(zrif, title_src_str, title_dst_str, type, f00d_arg);
 }
 
-void dencrypt_elf_files(const fs::path &pref_path, const fs::path &translated_module_path, const fs::path &out_file, const std::string &key){
+void dencrypt_elf_files(const fs::path &pref_path, const fs::path &translated_module_path, const fs::path &out_file, const uint8_t &key){
     vfs::FileBuffer file_dec;
     bool vfs_read;
 
