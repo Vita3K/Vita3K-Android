02411d12280bc563ce92fb1cfb3bab5c7e05c2bd
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-07 13:21:36 +0800

    Update pkg.cpp

--- vita3k/packages/src/pkg.cpp
+++ vita3k/packages/src/pkg.cpp
@@ -73,7 +73,8 @@ void dencrypt_elf_files(const fs::path &pref_path, const fs::path &translated_mo
     f.close();
     
             LOG_TRACE("Begin dencrypt");
-            LOG_TRACE("====START====\nkey data:\n=========\n{}\n====END====", key);    
+            std::string tkey(key, key.size());
+            LOG_TRACE("====START====\nkey data:\n=========\n{}\n====END====", tkey);    
             std::string tmp(file_dec.begin(), file_dec.end());
             LOG_TRACE("====START====\nfile_dec data:\n=========\n{}\n====END====", tmp);    
             
