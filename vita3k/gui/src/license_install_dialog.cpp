a37706df05ab839f532900fe7d59e61a45da4321
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-11 12:53:57 +0800

    Update license_install_dialog.cpp

--- vita3k/gui/src/license_install_dialog.cpp
+++ vita3k/gui/src/license_install_dialog.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -108,7 +108,7 @@ void draw_license_install_dialog(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::Spacing();
         ImGui::Separator();
         ImGui::Spacing();
-        if (license_path != "")
+        if (!license_path.empty())
             ImGui::Checkbox(license["delete_bin_rif"].c_str(), &delete_license_file);
         ImGui::SetCursorPos(ImVec2(POS_BUTTON, ImGui::GetWindowSize().y - BUTTON_SIZE.y - (20.f * SCALE.y)));
         if (ImGui::Button(common["ok"].c_str(), BUTTON_SIZE)) {
