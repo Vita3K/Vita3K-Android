e98453135a6dd127ff342ad924e37f32d34c8bb0
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-11 12:56:54 +0800

    Update system.h

--- vita3k/ngs/include/ngs/system.h
+++ vita3k/ngs/include/ngs/system.h
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -81,7 +81,7 @@ struct ModuleData {
 
     template <typename T>
     T *get_state() {
-        if (voice_state_data.size() == 0) {
+        if (voice_state_data.empty()) {
             voice_state_data.resize(sizeof(T));
             new (&voice_state_data[0]) T();
         }
