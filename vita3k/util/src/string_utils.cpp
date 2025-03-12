d453987ba9ed0205d5ab1d1e286f9b01b9d6a7ba
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-03-13 00:32:56 +0800

    Update string_utils.cpp

--- vita3k/util/src/string_utils.cpp
+++ vita3k/util/src/string_utils.cpp
@@ -15,7 +15,7 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#include <boost/static_string/static_string.hpp>
+#include <boost/container/string.hpp>
 
 #include <util/string_utils.h>
 
