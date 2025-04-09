c669df5b51499afc8ea0e756650632618dab313e
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2025-04-09 10:47:24 +0800

    Update SceVshBridge.cpp

--- vita3k/modules/SceVshBridge/SceVshBridge.cpp
+++ vita3k/modules/SceVshBridge/SceVshBridge.cpp
@@ -1,5 +1,5 @@
 // Vita3K emulator project
-// Copyright (C) 2024 Vita3K team
+// Copyright (C) 2025 Vita3K team
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -748,8 +748,7 @@ EXPORT(int, vshSblSsIsDevelopmentMode) {
 }
 
 EXPORT(int, vshSblUtMgrHasComTestFlag) {
-    STUBBED("");
-    return 1;
+    return UNIMPLEMENTED();
 }
 
 EXPORT(int, vshSblUtMgrHasNpTestFlag) {
